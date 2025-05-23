bdcc7034a0b271b46df08a601ac9f946aa3cbb98
Marco Bytes <mornsophannamis@gmail.com>
2025-05-23 13:35:10 +0700

    sonar scan

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,13 @@
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("org.sonarqube") version "4.4.1.3373"
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "android-kotlin")
+        property("sonar.host.url", "http://202.79.29.108:9000")
+        property("sonar.login", "sqp_7d5ea102d65f7a9f82a0578b4c32930d174afdce") // 🔐 uses GitHub Actions secret
+    }
 }
